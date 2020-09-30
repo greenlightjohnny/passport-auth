2162c781539ba618e716ddd38a7f58501135c094
fj <castelloiv@gmail.com>
2020-09-30 15:49:55 -0400

    css

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const main = require("./routes/index");
+const expressLayouts = require("express-ejs-layouts");
+const mainRoutes = require("./routes/index");
+const userRoutes = require("./routes/users");
 const app = express();
 
+/// EJS middleware
+app.use(expressLayouts);
+//Set view engine
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+//Routes!
+app.use("/", mainRoutes);
+app.use("/users/", userRoutes);
+
 const PORT = process.env.port || 5000;
-
-app.get("/", main);
-
 app.listen(PORT, console.log(`running on port ${PORT}`));
