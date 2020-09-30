50bdcc0fe86f8d3cfbb009bbafb6b654486e58dd
fj <castelloiv@gmail.com>
2020-09-30 19:00:02 -0400

    Mongoose connection and schema

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
 const mainRoutes = require("./routes/index");
 const userRoutes = require("./routes/users");
+const { MongoURI } = require("./config/keys");
 const app = express();
+require("dotenv").config();
+
+const mongURI = process.env.MONGO_URI;
 
 /// EJS middleware
 app.use(expressLayouts);
@@ -15,6 +20,17 @@ app.use("/public", express.static("public"));
 //Routes!
 app.use("/", mainRoutes);
 app.use("/users", userRoutes);
+
+////Connect to the database
+mongoose
+  .connect(MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => console.log("WE have connection to the DB"))
+  .catch((err) => console.log("%%%%%%%%%app.js", err));
+///Connect to the DB
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, console.log(`running on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb+srv://soso:soso@cluster0.1hf7x.mongodb.net/pass?retryWrites=true&w=majority",
+};
