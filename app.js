d68bfe5a103a8f85bfb8fc920f1e2edd9cc213be
fj <castelloiv@gmail.com>
2020-09-30 18:14:47 -0400

    fixed path

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ app.use(expressLayouts);
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static("public"));
 
 //Routes!
 app.use("/", mainRoutes);
-app.use("/users/", userRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.port || 5000;
 app.listen(PORT, console.log(`running on port ${PORT}`));
